0c35e3b52cbc53f6921705cbb1a231d2f187d23d
Guisoares28 <gui.soares2899@gmail.com>
2024-06-26 17:01:32 -0300

    Inserção do atributo status e criação da função delete

--- Sistem/Models/Conta.py
+++ Sistem/Models/Conta.py
@@ -1,31 +1,29 @@
 import decimal
+import logging
 from datetime import datetime
-from sqlalchemy import create_engine, Column, String, DECIMAL, Integer,DateTime
+from sqlalchemy import create_engine, Column, String, DECIMAL, Integer,DateTime,Boolean
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decimal import Decimal
 
 engine= create_engine('sqlite:///contas.db', echo= True)
 Base = declarative_base()
-#criando sessao
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
 class Conta(Base):
     __tablename__ = 'ContasAPagar'
 
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True,autoincrement=True, unique=True)
     descricao = Column(String)
     dataVencimento = Column(DateTime)
     valor = Column(DECIMAL)
+    status = Column(Boolean)
 
     def __repr__(self):
-        return f'Descrição: {self.descricao,} valor:{self.valor} Vencimento:{self.dataVencimento}'
+        return f'id: {self.id} Descrição: {self.descricao,} valor:{self.valor} Vencimento:{self.dataVencimento} Status:{self.status}'
 
      # Função para cadastrar usuarios no banco de dados.
     def cadastrarConta(self):
-
         print("****CADASTRO DE CONTAS****")
         descricao = input("Descrição da conta: ").lower()
         while(True):
@@ -48,21 +46,50 @@ class Conta(Base):
                         continue
                     else:
                         break
+                else:
+                    break
             except ValueError:
                 print("\nPor favor insira um data correta no formato dd/mm/aaaa !!!!")
         try:
-            newConta = Conta(descricao=descricao, valor=valor_decimal, dataVencimento=date_Obejct)
+            newConta = Conta(descricao=descricao, valor=valor_decimal, dataVencimento=date_Obejct, status=False)
             session.add(newConta)
             session.commit()
+            session.close()
             print("\nConta adicionada com sucesso...")
-        except Exception:
-            print("Houve algum problema ao tentar adicionar a conta! tente novamente")
+        except Exception as e:
+            logging.error(f"Ocorreu um erro: {str(e)}")
 
-
+    # Função para consultar todas as contas a pagar cadastradas
     def consultarTodasAsContas(self):
         contas = session.query(Conta).all()
         print("****CONTAS****")
         for conta in contas:
             print(conta)
             print("*"*100)
+        session.close()
         return contas
+
+    # Função para deletar contas por id
+    def deletarConta(self):
+        print("****CONTAS****")
+        contas = Conta.consultarTodasAsContas(self)
+        while(True):
+            while(True):
+                try:
+                    contaDeletada = int(input("\nQual id da conta deseja deletar: "))
+                    contaBuscada = session.query(Conta).filter_by(id=contaDeletada).first()
+                    break
+                except ValueError:
+                    print("\nid informado está incorreto !!!")
+            if contaBuscada:
+                session.delete(contaBuscada)
+                session.commit()
+                session.close()
+                print("\nConta deletada com Sucesso!!!")
+                break
+            else:
+                print("\nNão existe nenhuma conta vinculada a este id !!!")
+
+
+
+
